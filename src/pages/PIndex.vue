5bfa57f1ecd4acd37aa7dd7b1b9b98e00c2969f9
sanjarbarakayev <genius.s.1799@gmail.com>
2025-03-27 21:45:51 +0500

    feat: start template

--- src/pages/PIndex.vue
+++ src/pages/PIndex.vue
@@ -1,5 +1,25 @@
 <template>
-  <h1>Home Page</h1>
+  <section>
+    <h1>Forms</h1>
+
+    <div>
+      <div v-for="form in forms" :key="form.id">
+        <h2>{{ form.label }}</h2>
+        <ElInput v-if="form.type === 'input'" v-model="values[form.key]" />
+        <ElCheckbox v-if="form.type === 'checkbox'" v-model="values[form.key]" />
+        <ElRadio v-if="form.type === 'radio'" v-model="values[form.key]" />
+        <ElDate v-if="form.type === 'date'" v-model="values[form.key]" />
+        <ElSelect v-if="form.type === 'select'" v-model="values[form.key]" />
+      </div>
+    </div>
+  </section>
 </template>
 
-<script setup></script>
+<script setup lang="ts">
+import * as data from "@/data/forms.json"
+import { reactive } from "vue";
+
+const forms = data.forms
+
+const values = reactive({})
+</script>
