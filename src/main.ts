a23cc6fc6c609eefbed157282d350734c5f41cb4
sanjarbarakayev <sanjarbarakayev5@gmail.com>
2025-03-29 18:12:47 +0500

    chore: add Element Plus icons

--- src/main.ts
+++ src/main.ts
@@ -1,6 +1,7 @@
 import "element-plus/theme-chalk/index.css"
 import "@/assets/styles/main.css"
 import ElementPlus from "element-plus"
+import * as ElementPlusIconsVue from "@element-plus/icons-vue"
 
 import { createApp } from "vue"
 import App from "./App.vue"
@@ -10,4 +11,9 @@ const app = createApp(App)
 
 app.use(router)
 app.use(ElementPlus)
+
+for (const [key, component] of Object.entries(ElementPlusIconsVue)) {
+  app.component(key, component)
+}
+
 app.mount("#app")
